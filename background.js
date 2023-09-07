10e159f9d96db121fff485da3df22d87a9155afc
Eda OZDEMIR <edaozdemir@live.fr>
2023-09-07 16:08:13 +0200

    modif pour que le timer s'arrete

--- background.js
+++ background.js
@@ -2,23 +2,33 @@ chrome.alarms.create("postureTimer", {
   periodInMinutes: 1 / 60,
 });
 
+chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+  chrome.storage.local.set({
+    timer: "timer" in res ? res.timer : 0,
+    timeOption: "timeOption" in res ? res.timeOption : 1,
+    isRunning: "isRunning" in res ? res.isRunning : false,
+  });
+});
+
+//res.timer= récupéré dans le stockage local
+//res.timeOption : durée définie par l'utilisateur
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "postureTimer") {
-    chrome.storage.local.get(["timer", "isRunning"], (res) => {
+    chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
-        console.log(timer);
+        let isRunning = true;
+        if (timer === 60 * res.timeOption) {
+          timer = 0; //le timer est initilisé à 0
+          isRunning = false; //le timer n'est plus en cours d'exécution
+        }
+
+        //réinitialisation du minuteur
         chrome.storage.local.set({
           timer,
+          isRunning,
         });
       }
     });
   }
-});
-
-chrome.storage.local.get(["timer", "isRunning"], (res) => {
-  chrome.storage.local.set({
-    timer: "timer" in res ? res.timer : 0,
-    isRunning: "isRunning" in res ? res.isRunning : false,
-  });
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,30 @@
-const startTimerBtn = document.getElementById("start-timer-btn");
-startTimerBtn.addEventListener("click", () => {
+//fonction pour mettre à jour le minuteur
+
+function updateTime() {
+  //on récupère les 3 valeurs dans le storage local de chrome
+  chrome.storage.local.get(["timer", "timeOption", "isRunning"], (res) => {
+    const time = document.getElementById("time"); //pour afficher en html
+
+    //la variable minute et calcul des minutes restantes
+    const minutes = String(res.timeOption - Math.ceil(res.timer / 60)).padStart(
+      2,
+      "0"
+    );
+    //res.timeOption: C'est la valeur de l'option de temps provenant d'un objet stocké dans le stockage local de Chrome.res.timer: C'est la valeur du minuteur, également obtenue à partir de l'objet stocké dans le stockage local de Chrome.
+    let seconds = "00";
+    if (res.timer % 60 != 0) {
+      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
+    }
+    time.textContent = `${minutes}:${seconds}`;
+    startTimerBtn.textContent = res.isRunning ? "Pause Timer" : "Start Timer";
+  });
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+const startTimerBtn = document.getElementById("start-timer-btn"); //on stock dans la variable
+startTimerBtn.addEventListener("click", () => { //on detecte le click de l'utilisateur
   chrome.storage.local.get(["isRunning"], (res) => {
     chrome.storage.local.set(
       {
@@ -14,36 +39,14 @@ startTimerBtn.addEventListener("click", () => {
   });
 });
 
-const time = document.getElementById("time");
-function updateTime() {
-  chrome.storage.local.get(["timer"], (res) => {
-    const time = document.getElementById("time");
-
-    // get no. of minutes & secs
-    const minutes = `${1 - Math.ceil(res.timer / 60)}`.padStart(2, "0");
-    let seconds = "00";
-    if (res.timer % 60 != 0) {
-      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
-    }
-
-    // show minutes & secs on UI
-    time.textContent = `${minutes}:${seconds}`;
-  });
-}
-
-updateTime();
-setInterval(updateTime, 1000);
-
 const resetTimerBtn = document.getElementById("reset-timer-btn");
 resetTimerBtn.addEventListener("click", () => {
   chrome.storage.local.set(
     {
-      // reset variables
       timer: 0,
       isRunning: false,
     },
     () => {
-      // reset start button text-content
       startTimerBtn.textContent = "Start Timer";
     }
   );
