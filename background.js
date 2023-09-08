0d36bfe2e3b40cc8c7ea79af4fb6380e67ca4c0b
Eda OZDEMIR <edaozdemir@live.fr>
2023-09-08 10:17:40 +0200

    ajout api chrome.tabs pour ouvrir un nouvel onglet à 00:00

--- background.js
+++ background.js
@@ -2,23 +2,24 @@ chrome.alarms.create("postureTimer", {
   periodInMinutes: 1 / 60,
 });
 
-chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+chrome.storage.local.get(["timer", "isRunning", "startTimer"], (res) => {
   chrome.storage.local.set({
     timer: "timer" in res ? res.timer : 0,
-    timeOption: "timeOption" in res ? res.timeOption : 1,
+    timeStarter: "timeStarter" in res ? res.timeStarter : 1,
     isRunning: "isRunning" in res ? res.isRunning : false,
   });
 });
 
 //res.timer= récupéré dans le stockage local
-//res.timeOption : durée définie par l'utilisateur
+//res.timeStarter : durée définie par l'utilisateur
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "postureTimer") {
-    chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+    chrome.storage.local.get(["timer", "isRunning", "timeStarter"], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
         let isRunning = true;
-        if (timer === 60 * res.timeOption) {
+        if (timer === 60 * res.timeStarter) {
+          chrome.tabs.create({ url: "https://google.com" });
           timer = 0; //le timer est initilisé à 0
           isRunning = false; //le timer n'est plus en cours d'exécution
         }
